4e7623e9f6a9901a0bc3d815686554137a4db834
Priscila A. Neves <purishira@gmail.com>
2022-09-28 11:46:01 -0300

    Deploy

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//trabalhando com JavaScript em um arquivo externo
+
+console.log(nome, "chamando variável criada com var");
+
+var nome = "FIAP";
+
+escreve("exemplo de função nominal");
+
+//função nominal - udf
+function escreve(titulo){
+    alert(titulo);
+}
